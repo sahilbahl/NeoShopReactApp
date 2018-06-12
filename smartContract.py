55b142619dab45945c95ec3810e330cc7848b195
sahilbahl <bahlsahil28@gmail.com>
2018-06-12 22:53:32 +1200

    Remove getting product step from smart contract

--- smartContract.py
+++ smartContract.py
@@ -3,10 +3,6 @@ APPLICATION_UUID = 'bd65600d-8669-4903-8a14-af88203add38'
 
 def Main(operation, args):
     if operation != None:
-        if operation == 'GetProductListHash':
-            productList = Get(GetContext(), APPLICATION_UUID)
-            return productList
-
         if operation == 'UpdateProductListHash':
             Put(GetContext(), APPLICATION_UUID, args[0])
             return True
